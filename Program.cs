85d4f0dab9884301cf5c81aacf5d623a9f9dbaba
tarkul02 <70142061+tarkul02@users.noreply.github.com>
2024-06-24 12:04:03 +0700

    fix
    fix

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PostSap_GR_TR
+{
+    class Program
+    {
+        [STAThread]
+
+
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(true);
+            Application.Run(new BBL_test());
+            Application.Exit();
+        }
+    }
+}
